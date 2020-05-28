a0622e746c13d047e39ac682d64fdeb91dca4692
Kelly Dwan <ryelle@users.noreply.github.com>
2020-05-28 15:53:02 -0400

    Extract props functiton

--- src/codemods/remove-colors.js
+++ src/codemods/remove-colors.js
@@ -1,60 +1,7 @@
 const postcss = require( 'postcss' );
 const getColors = require( 'get-css-colors' );
 
-/* A size is any number, optionally with a unit. */
-const isSize = ( value ) => /^\d+\s?([a-zA-Z]+)?$/.test( value.trim() );
-
-/* If the value has a color in it, getColors will return an array. */
-const isColor = ( value ) => null !== getColors( value );
-
-/* A line style is from a set of values */
-const isLineStyle = ( value ) =>
-	-1 !==
-	[
-		'auto', // Technically only allowed on outline.
-		'none',
-		'hidden',
-		'dotted',
-		'dashed',
-		'solid',
-		'double',
-		'groove',
-		'ridge',
-		'inset',
-		'outset',
-	].indexOf( value );
-
-function explodeProp( decl ) {
-	switch ( decl.prop ) {
-		case 'border':
-		case 'border-top':
-		case 'border-right':
-		case 'border-bottom':
-		case 'border-left':
-		case 'outline':
-			const values = decl.value.split( ' ' );
-			values.forEach( ( value ) => {
-				if ( isLineStyle( value ) ) {
-					decl.before(
-						postcss.decl( { prop: decl.prop + '-style', value } )
-					);
-				} else if (
-					isSize( value ) ||
-					-1 !== [ 'thin', 'medium', 'thick' ].indexOf( value )
-				) {
-					decl.before(
-						postcss.decl( { prop: decl.prop + '-width', value } )
-					);
-				} else if ( isColor( value ) ) {
-					decl.before(
-						postcss.decl( { prop: decl.prop + '-color', value } )
-					);
-				}
-			} );
-			decl.remove();
-			break;
-	}
-}
+const explodeProp = require( './utils/explode-prop' );
 
 module.exports = postcss.plugin( 'remove-colors', function() {
 	return function( root ) {
